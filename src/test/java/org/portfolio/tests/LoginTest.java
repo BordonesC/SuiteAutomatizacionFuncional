2fee690bd5232488a69b9f29ef2f14a6526b7806
BordonesC <akerfeldt67@gmail.com>
2025-07-31 02:31:30 -0400

    Implementacion test de prueba loginInvalidCredentias

--- src/test/java/org/portfolio/tests/LoginTest.java
+++ src/test/java/org/portfolio/tests/LoginTest.java
@@ -1,4 +1,18 @@
 package org.portfolio.tests;
 
-public class LoginTest {
+import org.openqa.selenium.By;
+import org.testng.annotations.*;
+
+public class LoginTest extends BaseTest{
+
+    @Test
+    //test para login con credenciales inválidas
+    public void loginInvalidCredentials(){
+        driver.findElement(By.linkText("Signup / Login")).click();
+        driver.findElement(By.name("email")).sendKeys("user@novalido.com");
+        driver.findElement(By.name("password")).sendKeys("123456");
+        driver.findElement(By.xpath("//button[text()='Login']")).click();
+
+        assert driver.getPageSource().contains("Your email or password is incorrect!");
+    }
 }
