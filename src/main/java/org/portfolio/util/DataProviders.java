33ee8e614481b9d6fa5de688249139941dad85b8
BordonesC <akerfeldt67@gmail.com>
2025-08-03 07:11:10 -0400

    Implementación DataProviders

---
+++ src/main/java/org/portfolio/util/DataProviders.java
@@ -0,0 +1,29 @@
+package org.portfolio.util;
+
+import org.portfolio.models.RegisterData;
+import org.testng.annotations.DataProvider;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class DataProviders {
+
+    @DataProvider(name = "loginData")
+    public Object[][] loginData() {
+        String path = "src/test/java/resources/test_data/loginData.xlsx";
+        return ExcelUtils.getExcelData(path, "Hoja1");
+    }
+
+    @DataProvider(name = "signupData")
+    public Object[][] signupData() {
+        String path = "src/test/java/resources/test_data/signupData.xlsx";
+        List<Map<String, String>> data = ExcelUtils.getExcelDataAsListOfMap(path, "Hoja1");
+
+        Object[][] result = new Object[data.size()][1];
+        for (int i = 0; i < data.size(); i++) {
+            result[i][0] = data.get(i);
+        }
+        return result;
+    }
+}
